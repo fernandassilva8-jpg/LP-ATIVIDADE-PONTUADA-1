3ac6e05a264b0ab7bb6ef0632e94b492f4b59375
fernandassilva8-jpg <fernanda.s.silva8@ba.estudante.senai.br>
2026-03-11 14:17:58 -0300

    Update quinta_questao.py

--- quinta_questao.py
+++ quinta_questao.py
@@ -1,23 +1,18 @@
 import os
 
-os.system('cls')
+os.system ('cls')
 
-(' Solicitando dados -')
-Operacao = input('Digite uma operacao (+,-,*,/)')
+a = int(input('Digite por favor o primeiro valor: '))
+b = int(input('Digite por favor o segundo valor: '))
+operador = input('Digite o operador: ')
 
-A = float(input('Digite o primeiro número'))
-B = float(input('Digite o segundo número'))
+if operador == '*':
+    print(f'{a * b}')
+elif operador == '+':
+    print(f'{a + b}')
+elif operador == '-':
+    print(f'{a - b}')
+elif operador == '/':
+    print(f'{a / b}')
 
-if operacao == '+':
-    resultado = A + B
-elif subtracao== '-':
-    resultado =  A - B
-elif multiplicacao == ' * ':
-    resultado = A * B
-elif  divisao == '/':
-    resultado = A / B
-else:
-    print ('Operação inválida')
-    resultado:None
-if resultado is not None:
-    print('Resultado:', resultado)
+
